634750862faaa22b0e7d06ea33ab91094ce735f3
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-25 21:54:28 +0700

    complete hero components

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,11 +1,12 @@
 <script>
 	import SectionWrapper from './SectionWrapper.svelte';
 	import Header from './Header.svelte';
+	import CTAs from './CTAs.svelte';
 </script>
 
 <SectionWrapper>
 	<Header />
-	<div class="flex flex-col ga-10 flex-1 items-center justify-center pb-10 md:pb-14">
+	<div class="flex flex-col gap-10 flex-1 items-center justify-center pb-10 md:pb-14">
 		<h2
 			class="text-5xl sm:text-6xl md:text-7xl lg:text08xl max-w-[1200px] mx-auto w-full text-center font-semibold"
 		>
@@ -18,5 +19,21 @@
 				>personalized workouts, exercise explanations, analytics</span
 			> and much much more.
 		</p>
+		<CTAs />
+		<div class="flex items-center justify-center gap-2 text-base">
+			<p>4.6</p>
+			{#each [0, 1, 2, 3, 4] as index}
+				<div class="grid place-items-center relative">
+					<i class="fa-solid fa-star opacity-0"></i>
+					<div
+						class={'absolute top-0 left-0 grid place-items-center ' +
+							(index === 4 ? 'w-[60%] overflow-hidden' : ' ')}
+					>
+						<i class="fa-solid fa-star text-amber-400"></i>
+					</div>
+				</div>
+			{/each}
+			<p>500+</p>
+		</div>
 	</div>
 </SectionWrapper>
