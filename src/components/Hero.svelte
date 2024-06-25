db7f0c56ae9597a0027b392e3151435d20578911
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-25 09:43:42 +0700

    design layout of the web

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,5 +1,8 @@
 <script>
 	import SectionWrapper from './SectionWrapper.svelte';
+	import Header from './Header.svelte';
 </script>
 
-<SectionWrapper></SectionWrapper>
+<SectionWrapper>
+	<Header />
+</SectionWrapper>
