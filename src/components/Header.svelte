54e66fe542715ef92d3195b85ca9fe4f03542d8a
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-26 10:53:34 +0700

    reactive value to open and close modal

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { openModal } from '../store';
 </script>
 
 <header class="flex flex-col relative z-20">
@@ -6,7 +7,7 @@
 		<a href="/">
 			<h1 class="font-semibold">Swoley <span class="text-indigo-400">Moley</span></h1>
 		</a>
-		<button class="md:hidden grid place-items-center">
+		<button on:click={() => ($openModal = true)} class="md:hidden grid place-items-center">
 			<i class="fa-solid fa-bars"></i>
 		</button>
 		<nav class="hidden md:flex items-center gap-4 lg:gap-6">
