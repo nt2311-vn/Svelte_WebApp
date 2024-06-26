dda0382993ce69997e155dc39f8583f6a4e72705
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-26 10:53:16 +0700

    reactive value openModal

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,30 @@
 <script>
 	import '../app.css';
+	import Footer from '../components/Footer.svelte';
+	import { openModal } from '../store';
+	let y;
+	$: outerHeight = 0;
+
+	function reroute(href) {}
 </script>
 
+{#if $openModal}
+	<div
+		class="fixed top-0 left-0 w-screen h-screen border-b bg-white z-50 flex flex-col gap-8 p-5 px-8"
+	>
+		<div class="flex items-center justify-between gap-4 border-b pb-2">
+			<h1 class="font-semibold">
+				Swoley <span class="text-indigo-400">Moley</span>
+			</h1>
+			<button class="border-none outline-none" on:click={() => ($openModal = false)}>
+				<i class="fa-solid fa-xmark text-2xl"></i>
+			</button>
+		</div>
+	</div>
+{/if}
 <slot />
-
+<Footer />
+<svelte:window bind:scrollY={y} bind:outerHeight />
 <!-- 
 header 
 hero 
